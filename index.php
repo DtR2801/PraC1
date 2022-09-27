3be73d382b64d9acc70c169aec4aee806f6c17c9
David Rünge <d280103@icloud.com>
2022-09-27 11:26:51 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    
+    <head>
+        <title>Curio Software Developement</title>
+        <meta charset="UTF-8">
+        <meta name=>
+        <link rel="stylesheet" href="css/styleIndex.css">
+    </head>
+
+    <body>
+        
+        <header>
+            <img class="logoTop" src="img/Logos/curio-16-midden-groen-logo-rgb.png" width="130px" alt="Logo van Curio">
+            <h1>Software Developer</h1>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+        
+    </body>
+
+</html>
